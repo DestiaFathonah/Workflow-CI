94006213f20e8ebe5e2f33212fc521d7c474bec4
DestiaFathonah <fathsadhira@gmail.com>
2025-12-31 03:39:44 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,16 +1,13 @@
-#import libraries
 import mlflow
 import mlflow.sklearn
 
-mlflow.set_tracking_url("http://127.0.0.1:5000")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("heart-disease-classification")
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
-import mlflow
-import mlflow.sklearn
 
 #load dataset
 df = pd.read_csv("heart_preprocessed.csv")
@@ -23,10 +20,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-#aktifkan autolog
+#autolog
 mlflow.sklearn.autolog()
 
-#training
 with mlflow.start_run():
     model = RandomForestClassifier(
         n_estimators=100,
@@ -34,7 +30,6 @@ with mlflow.start_run():
     )
     model.fit(X_train, y_train)
 
-    #evaluasi
     y_pred = model.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
@@ -43,6 +38,47 @@ with mlflow.start_run():
     mlflow.log_metric("accuracy_manual", acc)
     mlflow.log_metric("f1_score_manual", f1)
 
+    import mlflow
+import mlflow.sklearn
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("heart-disease-classification")
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+
+df = pd.read_csv("heart_preprocessed.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy_manual", acc)
+    mlflow.log_metric("f1_score_manual", f1)
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model"
+    )
+    
     print("Accuracy:", acc)
     print("F1 Score:", f1)
