ed943f3678e41519c5246f45ac1ade61676e61bb
DestiaFathonah <fathsadhira@gmail.com>
2026-01-02 02:46:11 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,34 +1,38 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("file:./mlruns")
+# 1. Set experiment 
 mlflow.set_experiment("ci-training")
 
-df = pd.read_csv("heart_preprocessed.csv")
+# 2. Load dataset
+data = pd.read_csv("heart_preprocessed.csv")
 
-X = df.drop("target", axis=1)
-y = df["target"]
+X = data.drop("target", axis=1)
+y = data["target"]
 
+# 3. Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
+# 4. Model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
+# 5. Predict
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
 
-print("Training selesai")
+mlflow.log_param("model_type", "LogisticRegression")
+mlflow.log_metric("accuracy", acc)
+
+
+mlflow.sklearn.log_model(model, "model")
+
+print(f"Training selesai, accuracy: {acc}")
