c8b3ccc4840ef20f2ca5bd7254f4fcf85673ea0e
DestiaFathonah <fathsadhira@gmail.com>
2026-01-02 02:02:55 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,51 +1,34 @@
+import pandas as pd
 import mlflow
 import mlflow.sklearn
-import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score
 
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("ci-training")
 
-def main():
-    mlflow.set_tracking_uri("file:///tmp/mlruns")
-    mlflow.set_experiment("heart-disease-ci")
+df = pd.read_csv("heart_preprocessed.csv")
 
-    #load dataset
-    df = pd.read_csv("heart_preprocessed.csv")
+X = df.drop("target", axis=1)
+y = df["target"]
 
-    X = df.drop("target", axis=1)
-    y = df["target"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
     )
+    model.fit(X_train, y_train)
 
-    with mlflow.start_run():
-        model = RandomForestClassifier(
-            n_estimators=100,
-            random_state=42
-        )
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
 
-        model.fit(X_train, y_train)
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
 
-        y_pred = model.predict(X_test)
-
-        acc = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
-
-        mlflow.log_param("n_estimators", 100)
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_metric("f1_score", f1)
-        
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model"
-        )
-
-        print("Accuracy:", acc)
-        print("F1 Score:", f1)
-
-
-if __name__ == "__main__":
-    main()
+print("Training selesai")
