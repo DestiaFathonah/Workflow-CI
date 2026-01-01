011796c9780bc144f0e3a2b9db44cc55627fdcf3
DestiaFathonah <fathsadhira@gmail.com>
2026-01-02 01:31:31 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,14 +1,17 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score
 
+
 def main():
-    mlflow.set_tracking_uri("file:./mlruns")
+    mlflow.set_tracking_uri("file:///tmp/mlruns")
     mlflow.set_experiment("heart-disease-ci")
 
+    #load dataset
     df = pd.read_csv("heart_preprocessed.csv")
 
     X = df.drop("target", axis=1)
@@ -18,25 +21,31 @@ def main():
         X, y, test_size=0.2, random_state=42
     )
 
-    model = RandomForestClassifier(
-        n_estimators=100,
-        max_depth=5,
-        random_state=42
-    )
+    with mlflow.start_run():
+        model = RandomForestClassifier(
+            n_estimators=100,
+            random_state=42
+        )
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+        model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+        y_pred = model.predict(X_test)
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("max_depth", 5)
+        acc = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("f1_score", f1)
+        mlflow.log_param("n_estimators", 100)
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("f1_score", f1)
+        
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="model"
+        )
 
-    mlflow.sklearn.log_model(model, artifact_path="model")
+        print("Accuracy:", acc)
+        print("F1 Score:", f1)
+
 
 if __name__ == "__main__":
     main()
