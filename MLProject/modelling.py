0521c5db01d823341e0e985984cd4857edfa3e76
DestiaFathonah <fathsadhira@gmail.com>
2026-01-02 00:50:45 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,9 +1,8 @@
-import pandas as pd
 import mlflow
 import mlflow.sklearn
-
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
 
 def main():
@@ -19,29 +18,25 @@ def main():
         X, y, test_size=0.2, random_state=42
     )
 
-    with mlflow.start_run():
-        model = RandomForestClassifier(
-            n_estimators=100,
-            random_state=42
-        )
-        model.fit(X_train, y_train)
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=5,
+        random_state=42
+    )
 
-        y_pred = model.predict(X_test)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-        acc = accuracy_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-        mlflow.log_param("n_estimators", 100)
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_metric("f1_score", f1)
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("max_depth", 5)
 
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model"
-        )
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
 
-        print("Accuracy:", acc)
-        print("F1 Score:", f1)
+    mlflow.sklearn.log_model(model, artifact_path="model")
 
 if __name__ == "__main__":
     main()
